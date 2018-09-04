3d466923ff6ef24f0fcda1b2d96f0e1d8c046d3a
Michał Piotr Stankiewicz <ms335789@students.mimuw.edu.pl>
2018-09-04 15:06:52 +0200

    better version

--- extacl.c
+++ extacl.c
@@ -2,13 +2,16 @@
 
 #include <inttypes.h>
 #include <sys/types.h>
+#include <stdio.h>
 #include <sys/syscall.h>
 #include "extacl.h"
 
-int extacl_get_file(const char *path, struct extacl *extacl, size_t extacl_len) {
-  return syscall(EXTACL_GET_SYSCALL, path, extacl, extacl_len);
+int extacl_get_file(const char *path, struct extacl *val, size_t extacl_len) {
+    printf("%s, %p, %ld\n", path, val, extacl_len);
+    return syscall(EXTACL_GET_SYSCALL, path, val, extacl_len);
 }
 
 int extacl_set_file(const char *path, struct extacl *extacl, size_t extacl_len) {
+    printf("%s, %p, %ld\n", path, extacl, extacl_len);
     return syscall(EXTACL_SET_SYSCALL, path, extacl, extacl_len);
 }
--- test.c
+++ test.c
@@ -6,27 +6,51 @@
 
 int main(int argc, char **argv) {
 	int err;
-	extacl_t *buff;
-	size_t size = 1;
-	buff = malloc(sizeof(extacl_t) * size);
-	buff->type = EXTACL_USER_READ;
-	buff->_pad = 0;
-	buff->uid_gid = 42;
-	buff->range_start = 500;
-	buff->range_len = 500;
+	struct extacl *buff;
+	unsigned int size = 1;
+	char *file_name;
+	if (argc < 2) {
+		printf("too few args\n");
+		return 1;
+	}
+	file_name = argv[1];
+	printf("allocating %ld memory for size %ld and count %ld\n", sizeof(struct extacl) * 16, sizeof(struct extacl), 16);
+
+	buff = malloc(sizeof(struct extacl) * 16);
+	
+	if (!buff) {
+		perror("Failed to malloc\n");
+		return 1;
+	}
 	
 	if (strcmp(argv[2], "get") == 0) {
-		err = extacl_get_file(argv[1], buff, size);
+		err = extacl_get_file(file_name, buff, 16);
+		printf("God %d\n", err);
 	} else if ((strcmp(argv[2], "set") == 0)) {
-		err = extacl_set_file(argv[1], buff, size);
+		buff->type = EXTACL_USER_READ;
+		buff->_pad = 0;
+		buff->uid_gid = 42;
+		buff->range_start = 500;
+		buff->range_len = 500;
+	
+
+		err = extacl_set_file(file_name, buff, 2);
 	} else {
 			err = 1;
 			perror("Not known command");
 	}
+	printf("Did not fail\n");	
 	
+
+	printf("%d\n", buff->type);
+	printf("%d\n", buff->_pad);
+	printf("%d\n", buff->uid_gid);
+	printf("%d\n", buff->range_start);
+	printf("%d\n", buff->range_len);
 	if (err) {
 		perror("failed to get");
 		return 1;
 	}
+	
 	return 0;
 }
